31dd3fa59c79cd086dd08d5397f7b7de9d2158a4
zganzone <zganzone@gmail.com>
2025-12-12 00:30:45 +0200

    Update entrypoint.sh

--- pvpgn-server-docker/entrypoint.sh
+++ pvpgn-server-docker/entrypoint.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+set -e
 
-# Ако /etc/pvpgn е празна -> копираме дефолтните
+# Проверка и копиране на дефолтни конфигурации
 if [ ! -f /etc/pvpgn/bnetd.conf ]; then
     echo "Copying default /etc/pvpgn..."
     mkdir -p /etc/pvpgn
     cp -r /defaults/etc/* /etc/pvpgn/
 fi
 
-# Ако /var/pvpgn е празна -> копираме дефолтните
 if [ ! -f /var/pvpgn/users.dat ]; then
     echo "Copying default /var/pvpgn..."
     mkdir -p /var/pvpgn
     cp -r /defaults/var/* /var/pvpgn/
 fi
 
-# Стартира PvPGN
-echo "Starting PvPGN..."
-exec "$@"
+# Стартираме всички нужни сървиси в foreground
+echo "Starting PvPGN services..."
+
+bnetd -f &
+sleep 1
+d2cs &
+sleep 1
+d2dbs &
+sleep 1
+/usr/local/pvpgn/sbin/bntrackd \
+    -l /var/pvpgn/bntrackd.log \
+    -o /var/pvpgn/games.txt \
+    -u 3 -x -c true &
+
+# Очакваме всички процеси да работят (foreground)
+wait
